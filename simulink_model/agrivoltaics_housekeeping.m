0f7dec90a95d33b1999bab98893fda1f6731219d
ciepme <114676624+ciepme@users.noreply.github.com>
2026-02-17 23:00:14 -0500

    Busses

---
+++ simulink_model/agrivoltaics_housekeeping.m
@@ -0,0 +1,21 @@
+%% Clear
+clear;
+clc;
+
+%% Add Path
+
+addpath('C:\Users\ciepm\OneDrive\Documents\Github\agrivoltaics\simulink_model\modules');
+
+%%
+
+%open data dictionary busses
+data_dict = Simulink.data.dictionary.open('agrivoltaics_v1_data_dict.sldd');
+open_system("agrivoltaics_v1");
+
+%% create variables
+
+variable_a = [0 5; 1 5];
+variable_b = [0 6; 1 6];
+
+%%
+out = sim("agrivoltaics_v1.slx");
