0fb4ccaae96cf4cae3b40317fa1a23630cbd5527
CharlieLenk1 <clenk@umich.edu>
2026-02-21 14:10:37 -0500

    CO2 emissions module is working

--- simulink_model/agrivoltaics_variable_definition.m
+++ simulink_model/agrivoltaics_variable_definition.m
@@ -42,6 +42,18 @@ params.crop.k =  0.65; %light extinction coefficient, crop dependent
 params.crop.LAI = 3.0; %lead area index (sq meters of leaves per sq meter of ground, crop dependent
 params.crop.capital_cost = 1500; %USD per kW in net costs
 
+% environmental parameters
+base_dir = fileparts(mfilename('fullpath'));
+
+if isempty(base_dir) % in case there are issues with referencing the main dir
+    base_dir = pwd;
+end
+data_dir = fullfile(base_dir,'parameterData');
+ci_avg_hourly = get_july1_hourly_carbon_intensity(data_dir);
+ci_July_1 = ci_avg_hourly; 
+params.env.ci_marginal_hourly_miso = ci_July_1;
+
+
 %% 2. Design Variables
 
 % Panel layout variables
@@ -65,3 +77,18 @@ params_bus = slBus1;
 var_bus = slBus2; 
 
 %out = sim("agrivoltaics_v1.slx");
+
+function ci_avg_hourly = get_july1_hourly_carbon_intensity(data_dir)
+emissions_2024 = readtable(fullfile(data_dir, ...
+    'generated_emissions_MISO_20240701T0400_20240702T0359_co2_for_electricity_5m.csv'));
+emissions_2025 = readtable(fullfile(data_dir, ...
+    'generated_emissions_MISO_20250701T0400_20250702T0359_co2_for_electricity_5m.csv'));
+
+ci_2024_5min = emissions_2024.total_co2_marginal_intensity_lbs_per_mwh;
+ci_2025_5min = emissions_2025.total_co2_marginal_intensity_lbs_per_mwh;
+ci_avg_5min = (ci_2024_5min + ci_2025_5min) / 2;
+
+% 12 five-minute points per hour --> 24 hourly vals
+ci_avg_hourly = mean(reshape(ci_avg_5min, 12, []), 1).'; % making this a matrix, transposing, and taking average of each col corresponding to each hour
+% disp(ci_avg_hourly); % lbs/MWh
+end
