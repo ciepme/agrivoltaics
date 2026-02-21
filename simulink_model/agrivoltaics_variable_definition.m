bee7151b65bd867a1e5f502e160e77e5571e95e3
ciepme <114676624+ciepme@users.noreply.github.com>
2026-02-21 10:38:28 -0500

    Add Econ Variable

--- simulink_model/agrivoltaics_variable_definition.m
+++ simulink_model/agrivoltaics_variable_definition.m
@@ -1,7 +1,7 @@
 %% Clear
 clear;
 clc;
-%%
+
 %% Add Path
 
 addpath(genpath(pwd));
@@ -40,6 +40,7 @@ params.crop.MC = .85; %this is moisture content, raspberries are about 85% water
 params.crop.RUE = 2.0; %radiation use efficiency, g of biomass per MJ of light, crop dependent
 params.crop.k =  0.65; %light extinction coefficient, crop dependent
 params.crop.LAI = 3.0; %lead area index (sq meters of leaves per sq meter of ground, crop dependent
+params.crop.capital_cost = 1500; %USD per kW in net costs
 
 %% 2. Design Variables
 
