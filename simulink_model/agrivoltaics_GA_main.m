a492116b4d6c3ba4a474f5831dff8e6b2c7c455f
Jacob Cieply <114676624+ciepme@users.noreply.github.com>
2026-03-13 16:00:22 -0400

    New Constraints

--- simulink_model/agrivoltaics_GA_main.m
+++ simulink_model/agrivoltaics_GA_main.m
@@ -8,8 +8,8 @@ rng default;
 % Define the objective function and constraints
 agrivoltaic_wrapper = @(x) objectiveFunction(x);
 A = []; B = []; Aeq = []; Beq = [];
-constraint_min = [0 0 0 0 0 0 0 0];
-constraint_max = [2 2 2 pi./2 pi./2 0 10 10];
+constraint_min = [0 0 0 -pi -pi./2 -pi 0 0];
+constraint_max = [2 2 2 pi pi./2 pi 10 10];
 
 % Run GA
 [ga_solve,fval,exitflag,output,population,scores] = ga(@agrivoltaic_wrapper, ...
