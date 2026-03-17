e8b38d79a59f61aca0fdaaa14e57bd2638d926d2
CharlieL <charlie@zeroindustrial.energy>
2026-03-17 15:55:32 -0400

    Env. module now calculates total CI for a year using hourly data across the 4 seasonal days

--- simulink_model/agrivoltaics_variable_definition.m
+++ simulink_model/agrivoltaics_variable_definition.m
@@ -52,10 +52,21 @@ base_dir = fileparts(mfilename('fullpath'));
 if isempty(base_dir) % in case there are issues with referencing the main dir
     base_dir = pwd;
 end
-data_dir = fullfile(base_dir,'parameterData');
-ci_avg_hourly = get_july1_hourly_carbon_intensity(data_dir);
-ci_July_1 = ci_avg_hourly; 
-agriParams.env_ci_marginal_hourly_miso = ci_July_1;
+data_dir = fullfile(base_dir, 'parameterData');
+%for each of the solstice days--> timezone diff relative to UTC--> also
+%MISO only had marginal emissions data back to 2025 so I am just using the
+%2025 dats
+ci_jan21 = get_season_hourly_ci(data_dir,'generated_emissions_MISO_20250121T0500_20250122T0459_co2_for_electricity_5m.csv');
+ci_mar21 = get_season_hourly_ci(data_dir,'generated_emissions_MISO_20250321T0400_20250322T0359_co2_for_electricity_5m.csv');
+ci_jun21 = get_season_hourly_ci(data_dir,'generated_emissions_MISO_20250621T0400_20250622T0359_co2_for_electricity_5m.csv');
+ci_sep21 = get_season_hourly_ci(data_dir,'generated_emissions_MISO_20250921T0400_20250922T0359_co2_for_electricity_5m.csv');
+% Combine all seasons in order, starting from the first file passed
+% [96 x 1]
+ci_all_seasons = [ci_jan21; ci_mar21; ci_jun21; ci_sep21];
+% in lbs /MWh --> marginal CO2 emissions which should be about equal to (but a little less than) marginal CO2eq emissions
+agriParams.env_ci_marginal_hourly_miso = ci_all_seasons;
+clear ci_jan21 ci_sep21 ci_jun21 ci_mar21 ci_all_seasons;
+
 
 
 %% 2. Design Variables
@@ -82,17 +93,11 @@ var_bus = slBus2;
 %out = sim("agrivoltaics_v1.slx");
 save("agrivoltaics_variable_definition_data.mat")
 
-function ci_avg_hourly = get_july1_hourly_carbon_intensity(data_dir)
-emissions_2024 = readtable(fullfile(data_dir, ...
-    'generated_emissions_MISO_20240701T0400_20240702T0359_co2_for_electricity_5m.csv'));
-emissions_2025 = readtable(fullfile(data_dir, ...
-    'generated_emissions_MISO_20250701T0400_20250702T0359_co2_for_electricity_5m.csv'));
 
-ci_2024_5min = emissions_2024.total_co2_marginal_intensity_lbs_per_mwh;
-ci_2025_5min = emissions_2025.total_co2_marginal_intensity_lbs_per_mwh;
-ci_avg_5min = (ci_2024_5min + ci_2025_5min) / 2;
-
-% 12 five-minute points per hour --> 24 hourly vals
-ci_avg_hourly = mean(reshape(ci_avg_5min, 12, []), 1).'; % making this a matrix, transposing, and taking average of each col corresponding to each hour
-% disp(ci_avg_hourly); % lbs/MWh
+%% helper functions
+function ci_avg_hourly = get_season_hourly_ci(data_dir, file_season)
+    ci_data_season = readtable(fullfile(data_dir, file_season));
+    ci_season_day_5min = ci_data_season.total_co2_marginal_intensity_lbs_per_mwh;
+    % 12 five-minute points per hour --> 24 hourly vals--> in lbs/MWh
+    ci_avg_hourly = mean(reshape(ci_season_day_5min, 12, []), 1).';
 end
