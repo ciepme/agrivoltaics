bdcc07fd8580da885156a4ac77ad8bb0e07b4a9c
ciepme <114676624+ciepme@users.noreply.github.com>
2026-03-22 13:19:35 -0400

    GA Results

--- simulink_model/agrivoltaics_GA_main.m
+++ simulink_model/agrivoltaics_GA_main.m
@@ -2,19 +2,39 @@
 clear;
 clc;
 
-rng default;
-
 addpath(genpath(pwd));
 
 agrivoltaics_variable_definition;
 
+GA_SELECTOR = 4;
+
+%1 for basic GA
+if GA_SELECTOR == 1
+    rng(1);
+    options = optimoptions('ga', 'PopulationSize', 50, 'MaxGenerations', 40, ...
+        'FunctionTolerance', 1e-4,'MaxStallGenerations', 10,'Display', ...
+        'iter','PlotFcn', @gaplotbestf);
+elseif GA_SELECTOR == 2
+    rng(2);
+    options = optimoptions('ga', 'PopulationSize', 50, 'MaxGenerations', 100, ...
+        'FunctionTolerance', 1e-4,'MaxStallGenerations', 10,'Display', ...
+        'iter','PlotFcn', @gaplotbestf);
+elseif GA_SELECTOR == 3
+    rng(3);
+    options = optimoptions('ga', 'PopulationSize', 200, 'MaxGenerations', 100, ...
+        'FunctionTolerance', 1e-4,'MaxStallGenerations', 10,'Display', ...
+        'iter','PlotFcn', @gaplotbestf);
+elseif GA_SELECTOR == 4
+    rng(4);
+    options = optimoptions('ga', 'PopulationSize', 1800, 'MaxGenerations', 100, ...
+        'FunctionTolerance', 1e-4,'MaxStallGenerations', 10,'Display', ...
+        'iter','PlotFcn', @gaplotbestf);
+end
+
+
 % Set Up GA
 A = []; B = []; Aeq = []; Beq = [];
 nlcon = [];
-
-
-% set options
-options = optimoptions('ga', 'PopulationSize', 50, 'MaxGenerations', 100, 'FunctionTolerance', 1e-4,'MaxStallGenerations', 10,'Display', 'iter','PlotFcn', @gaplotbestf);
 
 % Run GA
 tic;
@@ -26,6 +46,7 @@ time_taken = toc;
 
 fprintf('Exit Flag: %d\n', exitflag);
 fprintf('Time Taken: %.2f seconds\n', time_taken);
+fprintf('Function Calls: %.0f\n', output.funccount);
 fprintf('Max Value: $%.2f\n', -fval);
 fprintf('Panel Height: %.2f m\n', ga_solve(1));
 fprintf('Panel Length: %.2f m\n', ga_solve(2));
