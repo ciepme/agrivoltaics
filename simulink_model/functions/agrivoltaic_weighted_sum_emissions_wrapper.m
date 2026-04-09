eb7bd740fd22bbed3274ce9d7a447d1422075201
ciepme <114676624+ciepme@users.noreply.github.com>
2026-04-09 11:18:39 -0400

    New Wrapper for Social Benefit

--- simulink_model/functions/agrivoltaic_weighted_sum_emissions_wrapper.m
+++ simulink_model/functions/agrivoltaic_weighted_sum_emissions_wrapper.m
@@ -1,4 +1,4 @@
-function cost = agrivoltaic_weighted_sum_wrapper(custom_var, agriParams, delta)
+function cost = agrivoltaic_weighted_sum_emissions_wrapper(custom_var, agriParams, delta)
     results = agrivoltaic_wrapper(custom_var, agriParams);
     E = results(1);
     P = results(2);
