0c1bad7236411270623fd0ca314ecd7213318dfd
ciepme <114676624+ciepme@users.noreply.github.com>
2026-04-11 23:42:47 -0400

    Pareto Done

--- simulink_model/combine_pareto.m
+++ simulink_model/combine_pareto.m
@@ -3,12 +3,31 @@ clear;
 close;
 clc;
 
+addpath(genpath(pwd));
+
+% prep model
+
+agrivoltaics_variable_definition;
+
+%modify parameters
+agriParams.crop_elec_price = 0.054; % changed from 0.5; 0.054 is new nominal
+
 load("agrivoltaic_multiobjective_GA_main_data_berry_pop50.mat", ...
-    "social_cost_set", "social_cost_pop", "yearly_biomass_set", "yearly_biomass_pop");
+    "social_cost_set", "social_cost_pop", "yearly_biomass_set", ...
+    "ga_final_pop_set", "ga_set", "yearly_biomass_pop");
+social_cost_set_50 = social_cost_set;
+social_cost_pop_50 = social_cost_pop;
+ga_set_50 = ga_set;
+ga_final_pop_set_50 = ga_final_pop_set;
 social_benefit_50 = [-social_cost_set; -social_cost_pop];
 yearly_biomass_50 = [yearly_biomass_set; yearly_biomass_pop];
 load("agrivoltaic_multiobjective_GA_main_data_berry_pop80.mat", ...
-    "social_cost_set", "social_cost_pop", "yearly_biomass_set", "yearly_biomass_pop");
+    "social_cost_set", "social_cost_pop", "yearly_biomass_set", ...
+    "ga_final_pop_set", "ga_set", "yearly_biomass_pop");
+social_cost_set_80 = social_cost_set;
+social_cost_pop_80 = social_cost_pop;
+ga_set_80 = ga_set;
+ga_final_pop_set_80 = ga_final_pop_set;
 social_benefit_80 = [-social_cost_set; -social_cost_pop];
 yearly_biomass_80 = [yearly_biomass_set; yearly_biomass_pop];
 
@@ -27,6 +46,18 @@ pareto_front = sortrows(pareto_front, 1);
 x_limit = [min(pareto_data(:,1)) max(pareto_data(:,1))];
 y_limit = [min(pareto_data(:,2)) max(pareto_data(:,2))];
 
+% find optimal design
+social_design = [1.05215 678.568];
+biomass_design = [0.349174 1317.21];
+balanced_design = [0.832745 840.469];
+
+tol = 1e-3;
+
+social_index = find(all(abs(pareto_front - social_design) < 1e-3, 2), 1);
+biomass_index = find(all(abs(pareto_front - biomass_design) < 1e-2, 2), 1);
+balanced_index = find(all(abs(pareto_front - balanced_design) < 1e-3, 2), 1);
+intesting_designs = [pareto_front(social_index,:); pareto_front(biomass_index,:); pareto_front(balanced_index,:)];
+
 front_size = 200;
 val_size = 50;
 utopia_size = 800;
@@ -40,9 +71,38 @@ ylim(y_limit);
 xlim(x_limit);
 scatter(pareto_data(:,1), pareto_data(:,2), val_size, 'black', 'filled');
 scatter(pareto_front(:,1), pareto_front(:,2), front_size, 'green', 'filled');
+scatter(intesting_designs(:,1), intesting_designs(:,2), front_size, 'blue', 'd', 'filled');
 scatter(x_limit(2), y_limit(2), utopia_size, 'cyan', 'filled', "pentagram");
 plot(pareto_front(:,1), pareto_front(:,2), 'green', 'LineWidth', 3);
-legend("Values from GA Population", "Pareto Front", "Utopia Point", 'Location','southwest');
+scatter(intesting_designs(:,1), intesting_designs(:,2), front_size, 'blue', 'd', 'filled');
+legend("Values from GA Population", "Pareto Front", "Designs of Interest", "Utopia Point", 'Location','southwest');
 figure_name = "graphs/official_pareto" + "_combined" + ".png";
 saveas(fig3,figure_name);
 hold off;
+
+%% Find Indices
+
+social_index_full = find(all(abs(pareto_data - social_design) < 1e-3, 2), 1);
+biomass_index_full = find(all(abs(pareto_data - biomass_design) < 1e-2, 2), 1);
+balanced_index_full = find(all(abs(pareto_data - balanced_design) < 1e-3, 2), 1);
+
+% balanced
+if balanced_index_full < length(social_benefit_50)
+    variable_balanced_index = balanced_index_full - length(ga_set_50);
+    variables_balanced = ga_final_pop_set_50(variable_balanced_index,:);
+    agrivoltaic_wrapper(variables_balanced, agriParams)
+end
+
+% social
+if social_index_full <= length(social_cost_set_50)
+    variable_social_index = social_index_full;
+    variables_social = ga_set_50(social_index_full,:);
+    agrivoltaic_wrapper(variables_social, agriParams)
+end
+
+% biomass
+if biomass_index_full > length(social_benefit_50)
+    variable_biomass_index = biomass_index_full - length(social_benefit_50)  - length(ga_set_80);
+    variables_biomass = ga_final_pop_set_80(variable_biomass_index,:);
+    agrivoltaic_wrapper(variables_biomass, agriParams)
+end
