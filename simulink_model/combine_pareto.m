31753506ee02b8ef4f3bd62c912429f3f968d08e
ciepme <114676624+ciepme@users.noreply.github.com>
2026-04-29 20:12:34 -0400

    Add Fiscal Pareto Front

--- simulink_model/combine_pareto.m
+++ simulink_model/combine_pareto.m
@@ -14,39 +14,43 @@ agriParams.crop_elec_price = 0.054; % changed from 0.5; 0.054 is new nominal
 
 load("agrivoltaic_multiobjective_GA_main_data_berry_pop50.mat", ...
     "social_cost_set", "social_cost_pop", "yearly_biomass_set", ...
-    "ga_final_pop_set", "ga_set", "yearly_biomass_pop");
+    "ga_final_pop_set", "ga_set", "yearly_biomass_pop", "P_pop", "P_set");
 social_cost_set_50 = social_cost_set;
 social_cost_pop_50 = social_cost_pop;
 ga_set_50 = ga_set;
 ga_final_pop_set_50 = ga_final_pop_set;
 social_benefit_50 = [-social_cost_set; -social_cost_pop];
 yearly_biomass_50 = [yearly_biomass_set; yearly_biomass_pop];
+P_50 = [P_set; P_pop];
 
 load("agrivoltaic_multiobjective_GA_main_data_berry_pop80.mat", ...
     "social_cost_set", "social_cost_pop", "yearly_biomass_set", ...
-    "ga_final_pop_set", "ga_set", "yearly_biomass_pop");
+    "ga_final_pop_set", "ga_set", "yearly_biomass_pop", "P_pop", "P_set");
 social_cost_set_80 = social_cost_set;
 social_cost_pop_80 = social_cost_pop;
 ga_set_80 = ga_set;
 ga_final_pop_set_80 = ga_final_pop_set;
 social_benefit_80 = [-social_cost_set; -social_cost_pop];
 yearly_biomass_80 = [yearly_biomass_set; yearly_biomass_pop];
+P_80 = [P_set; P_pop];
 
 load("agrivoltaic_multiobjective_GA_main_data_berry_pop100.mat", ...
     "social_cost_set", "social_cost_pop", "yearly_biomass_set", ...
-    "ga_final_pop_set", "ga_set", "yearly_biomass_pop");
+    "ga_final_pop_set", "ga_set", "yearly_biomass_pop", "P_pop", "P_set");
 social_cost_set_100 = social_cost_set;
 social_cost_pop_100 = social_cost_pop;
 ga_set_100 = ga_set;
 ga_final_pop_set_100 = ga_final_pop_set;
 social_benefit_100 = [-social_cost_set; -social_cost_pop];
 yearly_biomass_100 = [yearly_biomass_set; yearly_biomass_pop];
+P_100 = [P_set; P_pop];
 
 
-%% Combine Pareto Fronts
+%% Combine Pareto Fronts for Social Profit
 
 social_benefit = [social_benefit_50; social_benefit_80; social_benefit_100];
 yearly_biomass = [yearly_biomass_50; yearly_biomass_80; yearly_biomass_100];
+P = [P_50; P_80; P_100];
 
 pareto_data = [social_benefit yearly_biomass];
 pareto_data_inverse = -1.*pareto_data;
@@ -67,7 +71,7 @@ tol = 1e-3;
 social_index = find(all(abs(pareto_front - social_design) < 1e-3, 2), 1);
 biomass_index = find(all(abs(pareto_front - biomass_design) < 1e-2, 2), 1);
 balanced_index = find(all(abs(pareto_front - balanced_design) < 1e-3, 2), 1);
-intesting_designs = [pareto_front(social_index,:); pareto_front(biomass_index,:); pareto_front(balanced_index,:)];
+interesting_designs = [pareto_front(social_index,:); pareto_front(biomass_index,:); pareto_front(balanced_index,:)];
 
 front_size = 200;
 val_size = 50;
@@ -82,14 +86,14 @@ ylim(y_limit);
 xlim(x_limit);
 scatter(pareto_data(:,1), pareto_data(:,2), val_size, 'black', 'filled');
 scatter(pareto_front(:,1), pareto_front(:,2), front_size, 'green', 'filled');
-scatter(intesting_designs(:,1), intesting_designs(:,2), front_size, 'blue', 'd', 'filled');
+scatter(interesting_designs(:,1), interesting_designs(:,2), front_size, 'blue', 'd', 'filled');
 scatter(max(pareto_data(:,1)), max(pareto_data(:,2)), utopia_size, 'cyan', 'filled', "pentagram");
 plot([social_design(1) social_design(1) x_limit(2)],[y_limit(2) social_design(2) social_design(2)], 'r--', 'LineWidth', 1);
 plot([balanced_design(1) balanced_design(1) x_limit(2)],[y_limit(2) balanced_design(2) balanced_design(2)], 'g--', 'LineWidth', 1);
 plot([biomass_design(1) biomass_design(1) x_limit(2)],[y_limit(2) biomass_design(2) biomass_design(2)], 'b--', 'LineWidth', 1);
 plot(pareto_front(:,1), pareto_front(:,2), 'green', 'LineWidth', 3);
 scatter(max(pareto_data(:,1)), max(pareto_data(:,2)), utopia_size, 'cyan', 'filled', "pentagram");
-scatter(intesting_designs(:,1), intesting_designs(:,2), front_size, 'blue', 'd', 'filled');
+scatter(interesting_designs(:,1), interesting_designs(:,2), front_size, 'blue', 'd', 'filled');
 legend("Values from GA Population", "Pareto Front", "Designs of Interest", "Utopia Point", ...
     "Social Cost of Carbon Design Dominated Space", ...
     "Balanced Design Dominated Space", ...
@@ -125,3 +129,30 @@ if biomass_index_full > (length(social_benefit_50) + length(social_benefit_80))
     variables_biomass = ga_final_pop_set_100(variable_biomass_index,:);
     agrivoltaic_wrapper(variables_biomass, agriParams)
 end
+
+%% Combine Pareto Fronts for Fiscal Profit
+
+fiscal_pareto_data = [P yearly_biomass];
+fiscal_pareto_front = fiscal_pareto_data(pareto_indices,:);
+
+x_limit = [(min(fiscal_pareto_data(:,1)) - 0.05) (max(fiscal_pareto_data(:,1)) + 0.05)];
+y_limit = [(min(fiscal_pareto_data(:,2)) - 100) (max(fiscal_pareto_data(:,2)) + 100)];
+
+interesting_fiscal_designs = fiscal_pareto_data([social_index_full biomass_index_full balanced_index_full],:);
+
+fig4 = figure;
+theme(fig4,"light");
+hold on;
+title("Relation between Agricultural Output and Fiscal Profit");
+xlabel("Fiscal Profit ($M)");
+ylabel("Annual Raspberry Production (g/m^2)");
+ylim(y_limit);
+xlim(x_limit);
+scatter(fiscal_pareto_data(:,1), fiscal_pareto_data(:,2), val_size, 'black', 'filled');
+scatter(fiscal_pareto_front(:,1), fiscal_pareto_front(:,2), front_size, 'green', 'filled');
+scatter(interesting_fiscal_designs(:,1), interesting_fiscal_designs(:,2), front_size, 'blue', 'd', 'filled');
+legend("Values from GA Population",  "Pareto Front for Social Profit", "Designs of Interest", ...
+    'Location','northwest');
+figure_name = "graphs/official_pareto" + "_combined_fiscal" + ".png";
+saveas(fig4,figure_name);
+hold off;
