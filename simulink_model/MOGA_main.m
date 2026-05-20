8a1d0b650445c02f93ab0e08369cc8398cb6169c
ciepme <114676624+ciepme@users.noreply.github.com>
2026-05-20 10:21:01 -0400

    MOGA Fixed and Single Axis Runs

--- simulink_model/MOGA_main.m
+++ simulink_model/MOGA_main.m
@@ -22,12 +22,14 @@ agrivoltaics_variable_definition;
 
 %User define statements
 GA_SELECTOR = 1;
-file_suffix = "_berry_pop100";
 
 %GA hyperparameter settings
-pop_size = 20;
+pop_size = 300;
 max_gen = 20;
 stall = 1;
+mode = agriParams.tracking_mode;
+
+moniker = "pop" + pop_size + "_max_gen" + max_gen + "_mode" + mode;
 
 % Tell GA exactly how many variables we are optimizing (7 or 103, dependent
 %create a smart guess to seed GA population for if tracking mode
@@ -52,11 +54,79 @@ A = []; B = []; Aeq = []; Beq = [];
 nlcon = [];
 
 ga_final_pop_set = [];
-ga_set = ones(1,7);
 
-% Run GA
+if mode == 0
+    ga_set = ones(1,7);
+elseif mode == 1
+    ga_set = ones(1,103);
+end
+
+%% Run GA
+
+tic;
 
 [ga_solve,fval,exitflag,output,population,scores] = ...
     gamultiobj(@(x) moga_objective_wrapper(x, agriParams), ...
     num_vars, A, B, Aeq, Beq, lb, ub, ...
     nlcon, options);
+
+disp("Time (s) for MOGA: " + toc);
+
+%% Save Data
+
+save("results/MOGA_data_" + moniker + ".mat", "ga_solve", "fval", "exitflag", "output", "population", "scores");
+
+%% Plot
+
+front_size = 200;
+val_size = 50;
+utopia_size = 800;
+
+fig1 = figure;
+
+title("Pareto Fronts");
+
+subplot(3,1,1);
+
+theme(fig1,"light");
+
+
+hold on;
+xlabel("Fiscal Profit ($M)");
+ylabel("Annual Raspberry Production (g/m^2)");
+scatter(-scores(:,2), -scores(:,3), val_size, 'black', 'filled');
+scatter(-fval(:,2), -fval(:,3), front_size, 'green', 'filled');
+%scatter(star_x_position_social_profit, star_y_position_berry_production, utopia_size, 'cyan', 'filled', "pentagram");
+legend("Values from GA Population", "Weighted Sum GA Output", "Utopia Point", 'Location','southwest');
+%ylim([0 star_y_position_berry_production]);
+%xlim([0 star_x_position_social_profit]);
+hold off;
+
+subplot(3,1,2);
+
+hold on;
+xlabel("Fiscal Profit ($M)");
+ylabel("Emission Reduction (t CO2e)");
+scatter(-scores(:,2), -scores(:,1), val_size, 'black', 'filled');
+scatter(-fval(:,2), -fval(:,1), front_size, 'green', 'filled');
+%scatter(star_x_position_social_profit, star_y_position_berry_production, utopia_size, 'cyan', 'filled', "pentagram");
+legend("Values from GA Population", "Weighted Sum GA Output", "Utopia Point", 'Location','southwest');
+%ylim([0 star_y_position_berry_production]);
+%xlim([0 star_x_position_social_profit]);
+hold off;
+
+subplot(3,1,3);
+
+hold on;
+xlabel("Annual Raspberry Production (g/m^2)");
+ylabel("Emission Reduction (t CO2e)");
+scatter(-scores(:,3), -scores(:,1), val_size, 'black', 'filled');
+scatter(-fval(:,3), -fval(:,1), front_size, 'green', 'filled');
+%scatter(star_x_position_social_profit, star_y_position_berry_production, utopia_size, 'cyan', 'filled', "pentagram");
+legend("Values from GA Population", "Weighted Sum GA Output", "Utopia Point", 'Location','southwest');
+%ylim([0 star_y_position_berry_production]);
+%xlim([0 star_x_position_social_profit]);
+hold off;
+
+figure_name = "graphs/moga_pareto_" + moniker + ".png";
+saveas(fig1,figure_name);
